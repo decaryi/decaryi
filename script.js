7aec8cc98be8734ec603d604da55a57989a1bf39
decaryi <decaryi@web.de>
2023-04-18 18:27:17 +0200

    jojo

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // JavaScript-Code zur Erkennung von mobilen Geräten und Anpassung der Schriftgröße
-if (window.innerWidth <= 768 || window.innerHeight <= 768) {
+if (window.innerWidth <= 800 || window.innerHeight <= 800) {
     // Gerät hat maximale Bildschirmbreite oder -höhe von 768px (oder kleiner)
     document.querySelector('.overlay-link').style.fontSize = '80px'; // Schriftgröße für mobile Geräte anpassen
   }
