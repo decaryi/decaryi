aeb6e3ee6a14ba7b2e2000fe4ce9f7e040611f0f
decaryi <decaryi@web.de>
2023-04-18 18:23:39 +0200

    yo

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // JavaScript-Code zur Erkennung von mobilen Geräten und Anpassung der Schriftgröße
 if (window.innerWidth <= 768 || window.innerHeight <= 768) {
     // Gerät hat maximale Bildschirmbreite oder -höhe von 768px (oder kleiner)
-    document.querySelector('.overlay-link').style.fontSize = '14px'; // Schriftgröße für mobile Geräte anpassen
+    document.querySelector('.overlay-link').style.fontSize = '80px'; // Schriftgröße für mobile Geräte anpassen
   }
   
